69921e380263286d37624d35b500d46e2d999f08
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2023-12-30 18:59:53 +0100

    XTCASH

--- website_example/config.js
+++ website_example/config.js
@@ -1,14 +1,14 @@
-var api = "https://xte.trrxitte.com/api";
-let parentCoin = "traaitt";
+var api = "https://xtcash.trrxitte.com/api";
+let parentCoin = "traaittcash";
 var telegram = "https://t.me/mineTRRXITTE";
 var facebook = "https://www.facebook.com/TRRXITTE";
 var email = 'https://trrxitte.com'
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "https://explorer.traaitt.com/?hash={id}#blockchain_block";
+var blockchainExplorer = "https://interface.traaittcash.com/block.html?hash={id}";
 // var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "https://explorer.traaitt.com/?hash={id}#blockchain_transaction";
+var transactionExplorer = "https://interface.traaittcash.com/transaction.html?hash={id}";
 //var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
 
 var themeCss = "themes/custom.css";
